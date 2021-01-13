30a7ff03fd806f5d4fee076fe2ff3397a3475099
Wojciech Maziarz <Wojciech255Gmail.com>
2021-01-13 17:25:08 +0100

    6.2 Added kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error !!!");
+        }
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator simpleCalculator = new Calculator();
+        int result1 = simpleCalculator.add(5, 5);
+        int result2 = simpleCalculator.subtract(5,5);
+        System.out.println(result1);
+        System.out.println(result2);
+
+
+        if (result1 != 10){
+            System.out.println("Error ! ! ! !");
+        } else {
+            System.out.println("Test add OK");
+        }
+        if (result2 != 0 ) {
+            System.out.println("Error ! ! ! ! ! ! !");
+        } else {
+            System.out.println("Test subtract OK");
+        }
     }
 }
