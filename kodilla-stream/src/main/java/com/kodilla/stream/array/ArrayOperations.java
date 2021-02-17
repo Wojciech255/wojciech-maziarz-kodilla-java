ddfd55ce0ee42fa96f951bce561aefcfe9a26d02
Wojciech Maziarz <Wojciech255Gmail.com>
2021-02-17 18:05:20 +0100

    7.5 Obliczamy średnią funkcyjnie

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,8 +1,18 @@
 package com.kodilla.stream.array;
 
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
 public interface ArrayOperations {
 
-    public static void getAverage(int[] numbers){
+    public static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length)
+                .map(i -> numbers[i]) // i po wstawieniu np 0 wyswietla zerowy element tablicy
+                .forEach(System.out::println); // wypisywanie elementow tablicy
 
+        OptionalDouble average = IntStream.range(0, numbers.length) // poczytac o typie Optional double
+                .map(i -> numbers[i])
+                .average();
+        return average.getAsDouble();
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,4 +1,22 @@
 package com.kodilla.stream.array;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ArrayOperationsTestSuite {
+
+    @Test
+    void testGetAverage() {
+
+        //Given
+        int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+
+
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+        double expected = 9.5;
+
+        //Then
+        Assertions.assertEquals(average, expected);
+    }
 }
