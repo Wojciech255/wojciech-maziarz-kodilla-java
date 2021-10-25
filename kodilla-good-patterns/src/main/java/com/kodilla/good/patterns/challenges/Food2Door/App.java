3656e5975f1d9318b4e4017ebd2a9b8bd65c741a
Wojciech Maziarz <Wojciech255Gmail.com>
2021-10-25 12:52:51 +0200

    13.4 Food2Door

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/App.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/App.java
@@ -3,7 +3,7 @@ package com.kodilla.good.patterns.challenges.Food2Door;
 public class App {
     public static void main(String[] args) {
         Provider provider = new Provider("John","Rambo","JohnRambo@gmail.com");
-        Product product = new Product("TV",5);
+        Product product = new Product("apples",5);
         OrderRequest orderRequest = new OrderRequest(provider, product);
         ProductOrderService productOrderService = new ProductOrderService(new EmailInfo(), new Order(), new RepoToDataBase());
         productOrderService.process(orderRequest);
